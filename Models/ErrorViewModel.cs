fcbbf4803402b03cff0ebc94cadca3f42f9d2b85
Jeff Spurlock <jspurlock54@gmail.com>
2019-02-18 16:53:15 -0800

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Available_test3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
